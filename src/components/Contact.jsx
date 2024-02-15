744a310d5d0743704d019093458b747ca64e0055
Harshit-Ranjan1717 <dankcoder17@gmail.com>
2024-02-15 15:52:30 +0530

    added contact me

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,19 @@
-const Contact=()=>{
-    return <h1>Contact Us Page 😊😊</h1>
-}
+const Contact = () => {
+  return( 
+  <>
 
-export default Contact
+
+
+    <div className="w-[800px] h-[200px] mx-auto mt-24 bg-gradient-to-r from-cyan-500 to-purple-500 shadow-xl rounded-lg">
+    <a href="https://harshit1717portfolio.netlify.app/" target="_blank">
+        <h1 className="text-5xl m-4 pt-16 hover:text-white ">Click here to go to My Website</h1>
+    </a>
+
+    </div>
+
+  </>
+  
+  )
+};
+
+export default Contact;
